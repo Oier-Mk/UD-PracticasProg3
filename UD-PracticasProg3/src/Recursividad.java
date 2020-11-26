d1aaed5abd3fef7b4609d4870e157af8bd18f835
Oier-Mk <oiermentxaka@opendeusto.es>
2020-11-26 16:15:41 +0100

    20201126

---
+++ UD-PracticasProg3/src/Recursividad.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * @author Mentxaka
+ * 
+ * 4.1. Método invertirFrase, que recibe un String y lo devuelve invertido letra a letra, de forma recursiva.
+ * 4.2. Método invertirPalabras, que recibe un String y lo devuelve invertido palabra a palabra (considerando los separadores habituales espacio, tabulador, salto de línea, símbolos de puntuación), de forma recursiva.
+ * 4.3. Método longAHexa, que recibe un long y devuelve la conversión de ese long a hexadecimal, de forma recursiva.
+ * 4.4. Método sacaPalabras, que recibe un fichero de texto y devuelve un ArrayList de Strings con todas las palabras del fichero de texto en orden inverso a como aparecen, de forma recursiva.
+ * 4.5. Método ordenaQuick, que recibe un arraylist de Strings (por ejemplo las palabras del 4.4.) y devuelve ese arraylist ordenado alfabéticamente por el método quicksort, de forma recursiva (observa que puede haber palabras repetidas).
+ * 4.6. Método buscaPalabra, que recibe un arraylist de Strings ordenado y una palabra, y devuelve la posición en la que se encuentra esa palabra (si está repetida, la posición de la última ocurrencia). De forma recursiva y utilizando un proceso de coste logarítmico. 
+ *
+ */
+public class Recursividad {
+	public static void main(String[] args) {
+		System.out.println("recursividad");
+	}
+}
